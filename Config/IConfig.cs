4d7cf4fc45d566ef81a0e359ecb4def3ec3ff89d
Max Dinglinger <md@tecno.de>
2025-03-14 15:32:05 +0100

    Adding functionality

---
+++ Config/IConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecInvoice_PDF.Config
+{
+    public interface IConfig
+    {
+        string xmlFilePath { get; set; }
+        string pdfFilePath { get; set; }
+    }
+}
