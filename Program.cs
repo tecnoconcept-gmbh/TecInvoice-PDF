41fd02c9387d8597d6a1b8de7210da6829b8d84c
Max Dinglinger <md@tecno.de>
2025-03-14 15:32:05 +0100

    Adding functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Config.Net;
+using iText.Kernel.Pdf;
+using iText.Kernel.XMP;
+using iText.Kernel.XMP.Options;
+using NLog;
+using System.Runtime.CompilerServices;
+using TecInvoice_PDF.Config;
+using its = iText.Kernel;
+
+namespace TecInvoice_PDF
+{
+    internal class Program
+    {
+        private static Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder<IConfig>()
+                .UseCommandLineArgs()
+                .Build();
+
+            if (string.IsNullOrWhiteSpace(config.xmlFilePath) || string.IsNullOrWhiteSpace(config.pdfFilePath))
+            {
+                Logger.Error("Please provide the xml and pdf file paths.");
+                return;
+            }
+            Logger.Info("Adding XML to PDF...");
+            AddXmlToPdf(config.xmlFilePath, config.pdfFilePath);
+            Logger.Info("Successfully added XML to PDF.");
+        }
+        private static void AddXmlToPdf(string xmlFile, string pdfFile)
+        {
+            var tempFile = Path.GetTempFileName();
+
+            var reader = new its.Pdf.PdfReader(pdfFile);
+            var writer = new its.Pdf.PdfWriter(new FileStream(tempFile, FileMode.Create));
+            var pdfDoc = new its.Pdf.PdfDocument(reader, writer);
+
+            Logger.Info("Adding XMP Metadata...");
+            byte[] xmpFile = File.ReadAllBytes("TecFerd_BASIC.xmp");
+            XMPMeta xmp = XMPMetaFactory.ParseFromBuffer(xmpFile);
+            SerializeOptions options = new SerializeOptions();
+            options.SetUseCanonicalFormat(true);
+            pdfDoc.SetXmpMetadata(xmp, options);
+
+            Logger.Info("Attaching XML to the PDF...");
+            PdfDictionary parameters = new PdfDictionary();
+            parameters.Put(PdfName.ModDate, new PdfDate().GetPdfObject());
+            var fs = its.Pdf.Filespec.PdfFileSpec.CreateEmbeddedFileSpec(pdfDoc, File.ReadAllBytes(xmlFile), "factur-x.xml", "factur-x.xml", new PdfName("application/xml"), parameters, PdfName.Alternative);
+            pdfDoc.AddFileAttachment("ZUGFeRD Rechnungsspezifikation", fs);
+            PdfArray array = new PdfArray();
+            array.Add(fs.GetPdfObject().GetIndirectReference());
+            pdfDoc.GetCatalog().Put(PdfName.AF, array);
+
+            Logger.Info("Closing PDF...");
+            pdfDoc.Close();
+            reader.Close();
+
+            File.Delete(pdfFile);
+            File.Move(tempFile, pdfFile);
+        }
+    }
+}
